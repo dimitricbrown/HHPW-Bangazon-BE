5091a9acde187b987207a5d4e1b9d26d4c51e273
dimitricbrown <dimitricbrown@outlook.com>
2023-10-16 18:41:36 -0500

    Completed CRUD for Orders and Items.

--- Program.cs
+++ Program.cs
@@ -34,5 +34,120 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// ORDERS
+// Get All Orders
+app.MapGet("/api/orders", (HHPWDbContext db) =>
+{
+    return db.Orders.ToList();
+});
+
+// Get A Single Order
+app.MapGet("/api/orders/{orderId}", (HHPWDbContext db, int id) =>
+{
+    Orders order = db.Orders.FirstOrDefault(o => o.Id == id);
+    if (order == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(order);
+});
+
+// Create A New Order
+app.MapPost("/api/orders", (HHPWDbContext db, Orders order) =>
+{
+    db.Orders.Add(order);
+    db.SaveChanges();
+    return Results.Created($"/api/orders/{order.Id}", order);
+});
+
+// Update An Existing Order
+app.MapPut("/api/orders/{orderId}", (HHPWDbContext db, int id, Orders order) =>
+{
+    Orders orderToUpdate = db.Orders.SingleOrDefault(o => o.Id == id);
+    if (orderToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+    orderToUpdate.OrderName = order.OrderName;
+    orderToUpdate.CustomerPhone = order.CustomerPhone;
+    orderToUpdate.CustomerEmail = order.CustomerEmail;
+    orderToUpdate.Type = order.Type;
+    orderToUpdate.OrderStatus = order.OrderStatus;
+    orderToUpdate.PaymentType = order.PaymentType;
+    orderToUpdate.OrderTotal = order.OrderTotal;
+    orderToUpdate.Rating = order.Rating;
+    orderToUpdate.UserId = order.UserId;
+    db.SaveChanges();
+    return Results.NoContent();
+});
+
+// Delete An Existing Order 
+app.MapDelete("/api/orders/{orderId}", (HHPWDbContext db, int id) =>
+{
+    Orders order = db.Orders.SingleOrDefault(o => o.Id == id);
+    if (order == null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Orders.Remove(order);
+    db.SaveChanges();
+    return Results.NoContent();
+});
+
+// ITEMS
+// Get All Items
+app.MapGet("/api/items", (HHPWDbContext db) =>
+{
+    return db.Items.ToList();
+});
+
+// Get A Single Item
+app.MapGet("/api/items/{itemId}", (HHPWDbContext db, int id) =>
+{
+    Items item = db.Items.FirstOrDefault(i => i.Id == id);
+    if (item == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(item);
+});
+
+// Create A New Item
+app.MapPost("/api/items", (HHPWDbContext db, Items item) =>
+{
+    db.Items.Add(item);
+    db.SaveChanges();
+    return Results.Created($"/api/items/{item.Id}", item);
+});
+
+// Update An Existing Item
+app.MapPut("/api/items/{itemId}", (HHPWDbContext db, int id, Items item) =>
+{
+    Items itemToUpdate = db.Items.SingleOrDefault(i => i.Id == id);
+    if (itemToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+    itemToUpdate.Name = item.Name;
+    itemToUpdate.Price = item.Price;
+    db.SaveChanges();
+    return Results.NoContent();
+});
+
+// Delete An Existing Item 
+app.MapDelete("/api/items/{itemId}", (HHPWDbContext db, int id) =>
+{
+    Items item = db.Items.SingleOrDefault(i => i.Id == id);
+    if (item == null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Items.Remove(item);
+    db.SaveChanges();
+    return Results.NoContent();
+});
+
 app.Run();
 
